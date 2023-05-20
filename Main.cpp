7be44299a831a4aaf54e7e3e1156fdc7625103cc
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-05-20 11:15:49 +1000

    Add test general for horn form

--- Main.cpp
+++ Main.cpp
@@ -70,9 +70,11 @@ int main()
     KB = "p2=> p3; p3 => p1; c => e; b&e => f; f&g => h; p1=>d; p1&p3 => c; a; b; p2;";
     query = "d";
     test_horn(KB, query);
+    test_general(KB, query);
 
     query = "a";
     test_horn(KB, query);
+    test_general(KB, query);
 
 
     KB = "(a <=> (c => ~d)) & b & (b => a); c; ~f || g;";
